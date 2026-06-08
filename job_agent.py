c1d43b7c7b3ea013d6537b1da3360573b652c191
wf4jpfy7m6-alt <wf4jpfy7m6@privaterelay.appleid.com>
2026-06-08 13:40:09 +0200

    Update job_agent.py

--- job_agent.py
+++ job_agent.py
@@ -1,1 +1,16 @@
-print("HELLO")
+import os
+import requests
+
+token = os.getenv("JOB_TG_TOKEN")
+chat_id = os.getenv("JOB_TG_CHAT_ID")
+
+r = requests.post(
+    f"https://api.telegram.org/bot{token}/sendMessage",
+    json={
+        "chat_id": chat_id,
+        "text": "Railway test"
+    }
+)
+
+print(r.status_code)
+print(r.text)
